76a714fd84ac5120ba6a20212d0127797778e882
krishna <krishshah920@gmail.com>
2020-06-04 18:41:50 +0530

    complete

--- snake/food.js
+++ snake/food.js
@@ -1,7 +1,7 @@
 import {onSnake, expandSnake} from './snake.js'
 import { randomGridPosition } from './grid.js'
 let food = getRandomFoodposition()
-const EXPANSION_RATE = 1
+const EXPANSION_RATE = 5
 
 
 export function update() {
--- snake/game.js
+++ snake/game.js
@@ -9,7 +9,7 @@ const gameBoard = document.getElementById('game-board')
 function main(currentTime){
     if (gameOver){
        if(confirm("You lost.press ok to restart.")){
-           window.location = '/snake'
+           window.location = '/'
        }
        return 
     }
